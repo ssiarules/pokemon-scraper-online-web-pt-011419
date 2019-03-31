94158fdcae997296134b537ea6901e3eead93aea
Johnson Joseph <jhonsonj88@gmail.com>
2019-03-31 14:58:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,11 @@
 class Pokemon
 
+  attr_accessor :id, :name, :type, :db
+
   def initialize (id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
-    @db = db 
+    @db = db
   end
 end
